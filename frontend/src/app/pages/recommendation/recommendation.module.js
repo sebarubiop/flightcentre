08cebea2e7f297d6c0bf8a42d724fda1d67dff11
sebarubiop <sebarubiop@gmail.com>
2017-03-26 09:41:20 +1000

    recommendation

---
+++ frontend/src/app/pages/recommendation/recommendation.module.js
@@ -0,0 +1,20 @@
+
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.recommendation', [])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+        .state('recommendation', {
+          url: '/recommendation/:origin',
+          templateUrl: 'app/pages/recommendation/recommendation.html',
+          title: 'Recommendation',
+          controller: 'RecommendationCtrl',
+          controllerAs: 'rctrl'
+        });
+  }
+
+})();
